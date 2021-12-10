fe7b9678b099a2819cc5d88e7edc5e3ea0624d54
Dsuesca <92896091+Dsuesca@users.noreply.github.com>
2021-12-09 22:44:35 -0500

    Add files via upload

---
+++ plantillas/logo.php
@@ -0,0 +1,5 @@
+<!-- Preloader LOGO DE CARGANDO PRE-CARGA-->
+
+<div class="preloader flex-column justify-content-center align-items-center">
+            <img class="animation__shake" src="adml/AdminLTE-3.1.0/dist/img/logo_expo.png" alt="logo_expo" height="80" width="80">
+        </div>
---
+++ plantillas/notificaciones.php
@@ -0,0 +1,29 @@
+ <!-- Notifications Dropdown Menu Notificaciones -->
+
+ <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <!-- icon de campana -->
+                        <i class="far fa-bell"></i>
+                        <span class="badge badge-warning navbar-badge">15</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <span class="dropdown-item dropdown-header">15 Notifications</span>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-envelope mr-2"></i> 4 mensajes nuevos
+                            <span class="float-right text-muted text-sm">3 mins</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-users mr-2"></i> 8 solicitudes de vinculación
+                            <span class="float-right text-muted text-sm">12 hours</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-file mr-2"></i> 3 reportes nuevos
+                            <span class="float-right text-muted text-sm">2 days</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item dropdown-footer">Ver todas las notificaciones</a>
+                    </div>
+                </li>
